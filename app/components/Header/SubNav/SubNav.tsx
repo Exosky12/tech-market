bffd5d8f8a7764e51d94d3e2b7071674f99dc2d9
ExoskyCode <exoskyv2@gmail.com>
2023-04-02 16:42:16 +0200

    💄 [ UI ] SubNav desktop

--- app/components/Header/SubNav/SubNav.tsx
+++ app/components/Header/SubNav/SubNav.tsx
@@ -1,9 +1,21 @@
-import styles from '../../../Styles/header.module.scss'
+import styles from '../../../Styles/header.module.scss';
+import Link from 'next/link';
 
 export const SubNav = () => {
-  return (
-    <nav className={styles.SubNav}>
-      
-    </nav>
-  )
-}
+	return (
+		<nav className={styles.SubNav}>
+			<div className={styles.Links}>
+				<Link href=''>MacBook</Link>
+				<Link href=''>iMac</Link>
+				<Link href=''>Ordinateurs</Link>
+				<Link href=''>Pc Gamer</Link>
+				<Link href=''>Consoles de jeux</Link>
+				<Link href=''>Smartphones</Link>
+				<Link href=''>AirPods</Link>
+				<Link href=''>Casques</Link>
+				<Link href=''>iPad</Link>
+				<Link href=''>TV</Link>
+			</div>
+		</nav>
+	);
+};
